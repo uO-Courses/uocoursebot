41bc11e0b275b42b968a0c7f0a93fd64434713eb
キャンド <ann.mauduy.decius@gmail.com>
2023-05-26 15:34:07 -0400

    Register management commands

--- src/cmds/adm.py
+++ src/cmds/adm.py
@@ -4,7 +4,7 @@ from lib.utils import embed_gen, pretty_print_user
 
 etime = time.time()
 
-def register_runtime(tree, client, uid_to_courses, gu):
+def register_adm(tree, client, uid_to_courses, gu):
     
     @tree.command(name="runtime", description="View how long the bot has been running")
     async def slash_03(intr01: discord.Interaction, course_code: str):
