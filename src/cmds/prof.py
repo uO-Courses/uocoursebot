2d7710910c18d326f7df023966f68b13911f3c6b
キャンド <ann.mauduy.decius@gmail.com>
2023-05-26 18:02:20 -0400

    Add error message

--- src/cmds/prof.py
+++ src/cmds/prof.py
@@ -21,30 +21,35 @@ def register_prof(tree, client, uid_to_courses, gu):
 
             profs = list(filter(("Unknown").__ne__, profs))
 
-            sel = Select(placeholder="Select the professor",
-                         options=[
-                             discord.SelectOption(
-                                label=prof
-                             ) for prof in profs
-                         ])
+            if len(profs) != 0:
+
+                sel = Select(placeholder="Select the professor",
+                            options=[
+                                discord.SelectOption(
+                                    label=prof
+                                ) for prof in profs
+                            ])
+                
+                
+                async def sel_callback(intr02: discord.Interaction):
+
+                    selected_prof = sel.values[0]
+
+                    emb = embed_gen(title=f"{selected_prof}", color = 10181046)
+
+                    emb.add_field(name="Rating", value=f"{get_prof(profs[0])}/5")
+
+                    await intr02.response.send_message(embed=emb)
+
+                sel.callback = sel_callback
+
+                view = View()
+                view.add_item(sel)
+
+                await intr01.followup.send(view=view)
             
-            
-            async def sel_callback(intr02: discord.Interaction):
-
-                selected_prof = sel.values[0]
-
-                emb = embed_gen(title=f"{selected_prof}", color = 10181046)
-
-                emb.add_field(name="Rating", value=f"{get_prof(profs[0])}/5")
-
-                await intr02.response.send_message(embed=emb)
-
-            sel.callback = sel_callback
-
-            view = View()
-            view.add_item(sel)
-
-            await intr01.followup.send(view=view)
+            else:
+                await intr01.followup.send("Unfortunately, no professors are known for this course.")
 
         else:
             await intr01.followup.send(spmsg)
