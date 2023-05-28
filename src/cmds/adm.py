6d994d7b55e8d7fc9db238ced874ff25ea9e97e5
xxxAnn <ann.mauduy.decius@gmail.com>
2023-05-28 15:05:12 -0400

    improve runtime command

--- src/cmds/adm.py
+++ src/cmds/adm.py
@@ -8,4 +8,4 @@ def register_adm(tree, client, uid_to_courses, gu):
     
     @tree.command(name="runtime", description="View how long the bot has been running")
     async def slash_03(intr01: discord.Interaction):
-        await intr01.response.send_message(f"The bot has been running for {get_time(int(time.time() - etime))} seconds.")
+        await intr01.response.send_message(f"The bot has been running for {get_time(int(time.time() - etime))}.")
